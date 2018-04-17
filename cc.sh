18590af193312a84a5b89e7bc877ff3e145e9c38
Alexandre KARASSOULOFF <akarasso@e2r3p12.42.fr>
2018-04-17 09:40:00 +0200

    norm 42

--- cc.sh
+++
@@ -1,7 +0,0 @@
-make -C libft/
-#clang -Wall -Wextra -Werror -I libft/includes -o get_next_line.o -c get_next_line.c
-clang -I libft/includes -o get_next_line.o -c get_next_line.c
-#clang -Wall -Wextra -Werror -I libft/includes -o main.o -c main.c
-clang -I libft/includes -o main.o -c main.c
-#gcc -fsanitize=address -o test_gnl main.o get_next_line.o -I libft/includes -L libft/ -lft
-gcc -o test_gnl main.o get_next_line.o -I libft/includes -L libft/ -lft
